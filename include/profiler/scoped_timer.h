031d40c6386db8023802323697da0b66fcabaf3e
Shamal Lakshan <shamallakshan00600@gmail.com>
2026-05-14 12:46:59 +0530

    macros definitions and kill switch

--- include/profiler/scoped_timer.h
+++ include/profiler/scoped_timer.h
@@ -1,13 +1,46 @@
 #include <chrono>
-#include <iostream>
 
-class Timer
+#ifdef ENABLE_PROFILING
+    // scope macro : required name input
+    #define PROFILE_SCOPE(name) profiler::ScopedTimer _timer_##__LINE__(name)
+
+    // automated function macro : just the call is enough.
+    #define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCTION__)
+#else
+    // this else block resolves to nothing if profiling is disabled
+    #define PROFILE_SCOPE(name)
+    #define PROFILE_FUNCTION()
+#endif
+
+
+namespace profiler 
 {
-private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
-    std::chrono::time_point<std::chrono::high_resolution_clock> end;
-    std::chrono::time_point<std::chrono::high_resolution_clock> functionTime;
-public:
-    Timer();
-    ~Timer();
-};
+    class ScopedTimer
+    {
+    private:
+        const char* m_Name;
+        std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTime;
+
+    public:
+        // named constructor
+        ScopedTimer(const char* name);
+
+        // destructor
+        ~ScopedTimer() noexcept; // destructor should be disable of throws.
+
+        // delete copy constructor
+        ScopedTimer(const ScopedTimer&) = delete;
+
+        // delete copy assign oprator
+        ScopedTimer& operator=(const ScopedTimer&) = delete;
+
+        // delete move constructor
+        ScopedTimer(ScopedTimer&&) = delete;
+
+        //delete move assign operator
+        ScopedTimer& operator=(ScopedTimer&&) = delete;
+
+        // stop sequence isolated from destructor
+        void Stop();
+    };
+}
--- src/scoped_timer.cpp
+++ src/scoped_timer.cpp
@@ -1,15 +1,28 @@
 #include "../include/profiler/scoped_timer.h"
+#include <iostream>
 
-
-Timer::Timer()
+profiler::ScopedTimer::ScopedTimer(const char* name) : m_Name(name)
 {
-    this->start = std::chrono::high_resolution_clock::now();
+    m_StartTime = std::chrono::high_resolution_clock::now();
+
 }
 
-Timer::~Timer()
+profiler::ScopedTimer::~ScopedTimer() noexcept
 {
-    this->end = std::chrono::high_resolution_clock::now();
+    Stop();
+}
 
-    std::chrono::duration<double, std::milli> elapsed = this->end - this->start;
-    std::cout << "Function Time: " << elapsed.count() << " ms" << std::endl;
+void profiler::ScopedTimer::Stop()
+{
+    auto endTime = std::chrono::high_resolution_clock::now();
+
+    auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTime).time_since_epoch().count();
+
+    auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
+
+    auto duration = end - start;
+    double ms = duration * 0.001;
+
+    std::cout << "Function Name: " << this->m_Name << " | Function Time: " << duration << "us (" << ms << "ms)\n";
+
 }
